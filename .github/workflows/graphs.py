8f0f542625b57f2fdb38aca92663471609259226
fqueiro <66741500+fqueiro@users.noreply.github.com>
2020-06-11 20:29:58 +0100

    Update graphs.py

--- .github/workflows/graphs.py
+++ .github/workflows/graphs.py
@@ -15,7 +15,7 @@ concelhos_lx = {'name':'Lisboa','label':'Lisboa Norte','conc':['Lisboa','Loures'
 concelhos_aml = {'name':'AML','label':'Área Metropolitana de Lisboa','conc':['Lisboa','Loures','Odivelas','Sintra','Amadora','Cascais','Mafra','Oeiras','Vila Franca De Xira','Azambuja','Alcochete', \
                 'Almada','Barreiro','Moita','Montijo','Palmela','Setúbal','Sesimbra','Seixal']}
 concelhos_lxsul = {'name':'LxSul','label':'Lisboa Sul','conc':['Sesimbra','Seixal','Barreiro','Montijo']}
-concelhos_curia = {'name':'Curia','label':'Área Metropolitana de Tamengos','conc':['Anadia','Mealhada','Águeda','Cantanhede','Oliveira Do Bairro','Mortágua']}
+concelhos_curia = {'name':'Curia','label':'Área Metropolitana de Tamengos','conc':['Anadia','Mealhada','Coimbra','Aveiro','Águeda','Cantanhede','Oliveira Do Bairro','Mortágua']}
 concelhos_amp = {'name':'AMP','label':'Área Metropolitana do Porto','conc':['Arouca','Espinho','Gondomar','Maia','Matosinhos','Oliveira De Azeméis','Paredes','Porto', \
                    'Póvoa De Varzim','Santa Maria Da Feira','Santo Tirso','São João Da Madeira','Trofa','Vale De Cambra', \
                    'Valongo','Vila Do Conde','Vila Nova De Gaia']}
@@ -33,7 +33,7 @@ def plot_conc(concelhos):
     for concelho in concelhos['conc']:
         casos_wide['temp'] = gaussian_filter1d(casos_wide[concelho].diff(), sigma=3)
         #_ = plt.plot(casos_wide['Data'].tail(plot_days), casos_wide[concelho].diff().rolling(window=smoothing).mean().tail(plot_days))
-        _ = plt.plot(casos_wide['Data'].tail(plot_days), casos_wide['temp'].tail(plot_days), linewidth=3)
+        _ = plt.plot(casos_wide['Data'].tail(plot_days), casos_wide['temp'].tail(plot_days), linewidth=4)
         lgd = ax.legend(concelhos['conc'], prop={'size': 20}, loc='upper left', bbox_to_anchor=(1, 1), frameon=False)
         plt.xticks(plticks)
         ax.tick_params(axis="both", which="both", bottom=False, top=False, left=False, right=False)
