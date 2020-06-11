375fa2438d40530cc8f6901e8a8fda75cdd01865
fqueiro <66741500+fqueiro@users.noreply.github.com>
2020-06-11 19:02:55 +0100

    Update graphs.py

--- .github/workflows/graphs.py
+++ .github/workflows/graphs.py
@@ -11,27 +11,30 @@ casos_wide = pd.read_csv(str(Path(__file__).resolve().parents[2] / 'data_concelh
 casos_wide.columns = map(str.title, casos_wide.columns)
 plot_days = 1000
 smoothing = 7
-concelhos_lx = {'name':'Lisboa','conc':['Lisboa','Loures','Odivelas','Sintra','Amadora','Cascais','Mafra','Oeiras','Vila Franca De Xira','Azambuja']}
-concelhos_aml = {'name':'AML','conc':['Lisboa','Loures','Odivelas','Sintra','Amadora','Cascais','Mafra','Oeiras','Vila Franca De Xira','Azambuja','Alcochete', \
+concelhos_lx = {'name':'Lisboa','label':'Lisboa Norte','conc':['Lisboa','Loures','Odivelas','Sintra','Amadora','Cascais','Mafra','Oeiras','Vila Franca De Xira','Azambuja']}
+concelhos_aml = {'name':'AML','label':'Área Metropolitana de Lisboa','conc':['Lisboa','Loures','Odivelas','Sintra','Amadora','Cascais','Mafra','Oeiras','Vila Franca De Xira','Azambuja','Alcochete', \
                 'Almada','Barreiro','Moita','Montijo','Palmela','Setúbal','Sesimbra','Seixal']}
-concelhos_lxsul = {'name':'LxSul','conc':['Sesimbra','Seixal','Barreiro','Montijo']}
-concelhos_curia = {'name':'Curia','conc':['Anadia','Mealhada','Águeda','Cantanhede','Oliveira Do Bairro','Mortágua']}
-concelhos_amp = {'name':'AMP','conc':['Arouca','Espinho','Gondomar','Maia','Matosinhos','Oliveira De Azeméis','Paredes','Porto', \
+concelhos_lxsul = {'name':'LxSul','label':'Lisboa Sul','conc':['Sesimbra','Seixal','Barreiro','Montijo']}
+concelhos_curia = {'name':'Curia','label':'Área Metropolitana de Tamengos','conc':['Anadia','Mealhada','Águeda','Cantanhede','Oliveira Do Bairro','Mortágua']}
+concelhos_amp = {'name':'AMP','label':'Área Metropolitana do Porto','conc':['Arouca','Espinho','Gondomar','Maia','Matosinhos','Oliveira De Azeméis','Paredes','Porto', \
                    'Póvoa De Varzim','Santa Maria Da Feira','Santo Tirso','São João Da Madeira','Trofa','Vale De Cambra', \
                    'Valongo','Vila Do Conde','Vila Nova De Gaia']}
-round(len(list(casos_wide['Data'].tail(plot_days)))/10,0)
+concelhos_pt = {'name':'Portugal','label':'Portugal','conc':['Área Metropolitana de Lisboa','Área Metropolitana do Porto','Área Metropolitana de Tamengos']}
+
 plticks = list(casos_wide['Data'].tail(plot_days))[10::max(1,int(len(list(casos_wide['Data'].tail(plot_days)))/20))]
 
 plt.rc('xtick', labelsize=20)
 plt.rc('ytick', labelsize=20)
 
 def plot_conc(concelhos):
+    casos_wide[concelhos['label']] = casos_wide[concelhos['conc']].sum(axis = 1, skipna = True) 
     fig, ax = plt.subplots(figsize=(15,12))
+    #ylim = plt.ylim(0, 60)
     for concelho in concelhos['conc']:
         casos_wide['temp'] = gaussian_filter1d(casos_wide[concelho].diff(), sigma=3)
         #_ = plt.plot(casos_wide['Data'].tail(plot_days), casos_wide[concelho].diff().rolling(window=smoothing).mean().tail(plot_days))
         _ = plt.plot(casos_wide['Data'].tail(plot_days), casos_wide['temp'].tail(plot_days))
-        lgd = ax.legend(concelhos['conc'], prop={'size': 20}, bbox_to_anchor=(1, 1))
+        lgd = ax.legend(concelhos['conc'], prop={'size': 20}, loc='upper left', bbox_to_anchor=(1, 1))
         plt.xticks(plticks)
         fig.autofmt_xdate()
     PATH_TO_GRAPH = str(Path(__file__).resolve().parents[2]) + '/docs/' + concelhos['name']
@@ -41,3 +44,4 @@ def plot_conc(concelhos):
 plot_conc(concelhos_aml)
 plot_conc(concelhos_curia)
 plot_conc(concelhos_amp)
+plot_conc(concelhos_pt)
